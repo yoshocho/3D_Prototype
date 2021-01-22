4584f395aa89f2c39a9a544391449a2f2502441f
yosho_cho <choyosho165152987@gmail.com>
2021-01-22 17:02:57 +0900

    メニュー画面を追加した

---
+++ Assets/Script/menu_manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_manager : MonoBehaviour
+{
+
+    [SerializeField] GameObject menuObj = null;
+    int count = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        menuObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("change"))
+        {
+            count += 1;
+        }
+
+        if (count % 2 == 0)
+        {
+            menuObj.SetActive(true);
+            Time.timeScale = 0f;
+
+        }
+        else
+        {
+            menuObj.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
