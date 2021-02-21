c163b7024ec88a244d1d08e9127d9a3cb404a69a
yosho_cho <choyosho165152987@gmail.com>
2021-02-22 00:32:23 +0900

    クリアシーンを追加

--- Assets/Script/DestroyController.cs
+++ Assets/Script/DestroyController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class DestroyController : MonoBehaviour
 {
+    //[SerializeField] public GameObject targetObject = null;
     [SerializeField] float DestructionTime = 1f;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(this.gameObject, DestructionTime);
+       // this.transform.LookAt(targetObject, transform);
     }
 
     // Update is called once per frame
---
+++ Assets/Script/clear_controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class clear_controller : MonoBehaviour
+{
+    [SerializeField] string m_sceneToBeLoaded = "";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadSceneAsync(m_sceneToBeLoaded);
+        }
+    }
+
+}
