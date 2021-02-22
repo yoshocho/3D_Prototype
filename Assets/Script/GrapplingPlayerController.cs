ffde7a3fb7bd0c6af4f061facd8f53f9c22ba341
yosho_cho <choyosho165152987@gmail.com>
2021-02-22 22:27:56 +0900

    ゲームシーンでのカットシーン中playerを動かせないようにした

--- Assets/Script/GrapplingPlayerController.cs
+++ Assets/Script/GrapplingPlayerController.cs
@@ -43,6 +43,14 @@ public class GrapplingPlayerController : MonoBehaviour
     EnemyDetector m_enemyDetector = null;
     //private bool mouseClick = false;
 
+    [SerializeField] bool m_PlayerOperation = true;
+
+    public bool PlayerOperation 
+    {
+        get { return m_PlayerOperation; }
+        set { m_PlayerOperation = value; }        
+    }
+
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
@@ -51,91 +59,95 @@ public class GrapplingPlayerController : MonoBehaviour
         m_attackTrigger.gameObject.SetActive(false);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-
+        
     }
 
     void Update()
     {
-        //mouseClick = Input.GetMouseButtonDown(0);
-        // 方向の入力を取得し、方向を求める
-        float v = Input.GetAxisRaw("Vertical");
-        float h = Input.GetAxisRaw("Horizontal");
-
-        // 入力方向のベクトルを組み立てる
-        Vector3 dir = Vector3.forward * v + Vector3.right * h;
-
-        // カメラを基準に入力が上下=奥/手前, 左右=左右にキャラクターを向ける
-        dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する
-        dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
-
-        if (IsGrounded())
+        if (m_PlayerOperation)
         {
-            if (dir == Vector3.zero)
+            float v = Input.GetAxisRaw("Vertical");
+            float h = Input.GetAxisRaw("Horizontal");
+
+            // 入力方向のベクトルを組み立てる
+            Vector3 dir = Vector3.forward * v + Vector3.right * h;
+
+            // カメラを基準に入力が上下=奥/手前, 左右=左右にキャラクターを向ける
+            dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する
+            dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
+
+            if (IsGrounded())
             {
-                // 方向の入力がニュートラルの時は、y 軸方向の速度を保持するだけ
-                m_rb.velocity = new Vector3(0f, m_rb.velocity.y, 0f);
+                if (dir == Vector3.zero)
+                {
+                    // 方向の入力がニュートラルの時は、y 軸方向の速度を保持するだけ
+                    m_rb.velocity = new Vector3(0f, m_rb.velocity.y, 0f);
+                }
+                else
+                {
+                    // 入力方向に滑らかに回転させる
+                    Quaternion targetRotation = Quaternion.LookRotation(dir);
+                    this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * m_turnSpeed);  // Slerp を使うのがポイント
+
+                    Vector3 velo = dir.normalized * m_movingSpeed; // 入力した方向に移動する
+                    velo.y = m_rb.velocity.y;   // ジャンプした時の y 軸方向の速度を保持する
+                    m_rb.velocity = velo;   // 計算した速度ベクトルをセットする
+                }
+
+                // ジャンプの入力を取得し、接地している時に押されていたらジャンプする
+                if (Input.GetButtonDown("Jump"))
+                {
+                    m_rb.AddForce(Vector3.up * m_jumpPower, ForceMode.Impulse);
+                    if (m_anim)
+                    {
+                        m_anim.SetTrigger("Jump");
+                    }
+                }
+
+
+
+                // 攻撃する
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    if (m_enemyDetector.Target)
+                    {
+                        this.transform.LookAt(m_enemyDetector.Target.transform);
+                    }
+
+                    if (m_anim)
+                    {
+                        m_anim.SetTrigger("Attack");
+
+                    }
+                }
+                //skillの発動
+                if (Input.GetButtonDown("Skill"))
+                {
+                    if (m_enemyDetector.Target)
+                    {
+                        this.transform.LookAt(m_enemyDetector.Target.transform);
+
+                    }
+                    if (m_anim)
+                    {
+                        m_anim.SetTrigger("SkillAttack");
+                    }
+                }
             }
             else
             {
-                // 入力方向に滑らかに回転させる
-                Quaternion targetRotation = Quaternion.LookRotation(dir);
-                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * m_turnSpeed);  // Slerp を使うのがポイント
-
-                Vector3 velo = dir.normalized * m_movingSpeed; // 入力した方向に移動する
-                velo.y = m_rb.velocity.y;   // ジャンプした時の y 軸方向の速度を保持する
-                m_rb.velocity = velo;   // 計算した速度ベクトルをセットする
-            }
-
-            // ジャンプの入力を取得し、接地している時に押されていたらジャンプする
-            if (Input.GetButtonDown("Jump"))
-            {
-                m_rb.AddForce(Vector3.up * m_jumpPower, ForceMode.Impulse);
-                if (m_anim)
+                // 空中にいる場合は、操作している方向に力を加える
+                Vector3 velo = m_rb.velocity;
+                velo.y = 0;
+                if (velo.magnitude < m_movingSpeed)
                 {
-                    m_anim.SetTrigger("Jump");
-                }
-            }
-
-
-
-            // 攻撃する
-            if (Input.GetButtonDown("Fire1"))
-            {
-                if (m_enemyDetector.Target)
-                {
-                    this.transform.LookAt(m_enemyDetector.Target.transform);
-                }
-
-                if (m_anim)
-                {
-                    m_anim.SetTrigger("Attack");
-
-                }
-            }
-            //skillの発動
-            if (Input.GetButtonDown("Skill"))
-            {
-                if (m_enemyDetector.Target)
-                {
-                    this.transform.LookAt(m_enemyDetector.Target.transform);
-
-                }
-                if (m_anim)
-                {
-                    m_anim.SetTrigger("SkillAttack");
+                    m_rb.AddForce(dir.normalized * m_movingPowerInTheAir);
                 }
             }
         }
-        else
-        {
-            // 空中にいる場合は、操作している方向に力を加える
-            Vector3 velo = m_rb.velocity;
-            velo.y = 0;
-            if (velo.magnitude < m_movingSpeed)
-            {
-                m_rb.AddForce(dir.normalized * m_movingPowerInTheAir);
-            }
-        }
+        //mouseClick = Input.GetMouseButtonDown(0);
+        // 方向の入力を取得し、方向を求める
+        
     }
 
     void LateUpdate()
